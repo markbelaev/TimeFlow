e346234ca5bfd1b5c11d5644b211877c69edd52a
Mark Belaev <markbelaevv@gmail.com>
2025-10-03 18:52:41 +0500

    добавил дев мод в конфиге

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,6 +1,7 @@
 package config
 
 import (
+	"flag"
 	"log/slog"
 	"os"
 
@@ -10,10 +11,15 @@ import (
 // Структура с токеном
 type Config struct {
 	Token string
+	isDev bool
 }
 
 // Функция врзвращающая токен
 func Load() *Config {
+
+	// Парсинг флага для DEV бота
+	devMode := flag.Bool("dev", false, "enable development mode")
+	flag.Parse()
 
 	// Загрузка .env файла
 	if err := godotenv.Load(".env"); err != nil {
@@ -21,15 +27,24 @@ func Load() *Config {
 		os.Exit(1)
 	}
 
-	// Проверка токена
-	tokenBot := os.Getenv("TOKEN_BOT")
+	// Выбор токена в зависимости от режима
+	var tokenBot string
+	if *devMode {
+		tokenBot = os.Getenv("TOKEN_BOT_DEV")
+		slog.Info("Running in DEV mode")
+	} else {
+		tokenBot = os.Getenv("TOKEN_BOT")
+		slog.Info("Running in PROD mode")
+	}
+
+	// Проверка если токен пустой
 	if tokenBot == "" {
-		slog.Error("TOKEN_BOT is empty")
-		os.Exit(1)
+		slog.Error("Token is empty")
 	}
 
 	// Возвращение структуры с токеном
 	return &Config{
 		Token: tokenBot,
+		isDev: *devMode,
 	}
 }
