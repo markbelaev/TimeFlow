2aec55532242231a82eeff6dc500a7472b47b999
Mark Belaev <markbelaevv@gmail.com>
2025-10-04 14:11:43 +0500

    добавил получение userID

--- internal/handlers/commands/commands.go
+++ internal/handlers/commands/commands.go
@@ -9,12 +9,16 @@ import (
 
 // Функция обработки команды /start
 func HandleStart(c telebot.Context) error {
-	slog.Info("successful command processing /start")
+	user := c.Sender()
+	slog.Info("Обработка /start", "userID", user.ID)
+
 	return c.Send(data.StartMessage)
 }
 
 // Обработчик команды /about
 func HandleAbout(c telebot.Context) error {
-	slog.Info("successful command processing /about")
+	user := c.Sender()
+	slog.Info("Обработка /about", "userID", user.ID)
+
 	return c.Send(data.AboutMessage)
 }
