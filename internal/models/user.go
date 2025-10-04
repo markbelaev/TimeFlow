006da15422860b1dd5367752bae28cd61bc0fd0e
Mark Belaev <markbelaevv@gmail.com>
2025-10-04 14:24:44 +0500

    добавил модель и получение информации пользователя

---
+++ internal/models/user.go
@@ -0,0 +1,19 @@
+package models
+
+import "gopkg.in/telebot.v4"
+
+type TgUser struct {
+	ID        int
+	FirstName string
+	LastName  string
+	Username  string
+}
+
+func GetUserInfo(user *telebot.User) TgUser {
+	return TgUser{
+		ID:        int(user.ID),
+		FirstName: user.FirstName,
+		LastName:  user.FirstName,
+		Username:  user.Username,
+	}
+}
